8337b419efcb3f50f302ee053c6a07b5904afde2
Brandon Brown <brandonb@redhillstudios.com>
2013-07-24 13:23:49 -0700

    initial commit (base scaffold)

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('unemploymentApp', [])
+  .config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
