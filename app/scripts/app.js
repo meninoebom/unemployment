f3d8e870f4dbec5626d70237791131eb85d02f41
Brandon Brown <brandonb@redhillstudios.com>
2013-07-25 17:47:39 -0700

    added state manager for routing and ng-class directive to body tag to manage base class for styling

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,13 +1,39 @@
 'use strict';
 
-angular.module('unemploymentApp', [])
-  .config(['$routeProvider', function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
+angular.module('unemploymentApp', ['ui','ui.state'])
+  .config(['$stateProvider','$routeProvider', function ($stateProvider, $routeProvider) {
+    $stateProvider
+      .state('intro', {
+        url: "",
+        templateUrl: "views/intro.html",
+        baseClass: "intro"
       })
-      .otherwise({
-        redirectTo: '/'
-      });
-  }]);
+      .state('overview', {
+        url: "/overview",
+        templateUrl: "views/overview.html",
+        baseClass: "intro"
+      })
+      .state('level-1-intro', {
+      url: "/level/1/intro",
+        templateUrl: "views/level-1-intro.html",
+        baseClass: "intro"
+      })
+      .state('level-1', {
+        url: "/level/1",
+        templateUrl: "views/level-1.html",
+        baseClass: "level-1"
+      })  ;
+
+
+    // $routeProvider
+    //   .when('/', {
+    //     templateUrl: 'views/main.html',
+    //     controller: 'MainCtrl'
+    //   })
+    //   .otherwise({
+    //     redirectTo: '/'
+    //   });
+  }]).run(function ($rootScope, $state, $stateParams) {
+    $rootScope.$state = $state;
+    $rootScope.$stateParams = $stateParams;
+});
