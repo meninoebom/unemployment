1c2497f21dc9f18da23a2722fb3ae2e086d9d87b
Brandon Brown <brandonb@redhillstudios.com>
2013-08-06 17:14:59 -0700

    incorrect popup working on blur with fake content

--- app/scripts/directives/questionDirectives.js
+++ app/scripts/directives/questionDirectives.js
@@ -5,11 +5,22 @@ angular.module('questionDirectives', [])
     return {
 		restrict: 'A',
 		link: function(scope, elem, attrs, ctrl) {
-			var content = '<img src="img/laborForceParticipationFormula.png" width="311" height="149" />'
-			
-
-
-			elem.popover({ content: content, trigger: "click", html: true, placement: "bottom" });
+			var popverContent = {
+				laborForceParticipation: '<img src="img/laborForceParticipationFormula.png" width="311" height="149" />'
+			}
+			var content = popverContent[attrs.content];
+			elem.popover({ content: content, trigger: "hover", html: true, placement: "bottom" });
+		}
+	};
+}).directive('showWrongRight', function(){
+    return {
+		restrict: 'A',
+		link: function(scope, elem, attrs, ctrl) {
+			elem.popover({ content: 'test', trigger: "manual", html: true, placement: "top", 
+				template: '<div class="popover incorrect-answer-popover"><div class="arrow"></div><div class="popover-inner"><h3 class="popover-title"></h3><div class="popover-content"><p></p></div></div></div>' });
+			elem.bind('blur', function() {
+				elem.popover('show');
+			});
 		}
 	};
 }).directive('eatClick', function() {
