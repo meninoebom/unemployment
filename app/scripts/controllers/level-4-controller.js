457accce2739d60b65cab726666db0cffc7561a3
Brandon Brown <bbrown@johnmcneilstudio.com>
2013-08-21 17:26:40 -0700

    level 4 period selector styles

--- app/scripts/controllers/level-4-controller.js
+++ app/scripts/controllers/level-4-controller.js
@@ -3,18 +3,18 @@
 unemploymentApp.controller('Level4Ctrl', ['$scope',  function($scope ) {
 	
 	$scope.recessions = [
-		{name: "Great Depression: August 1929 - March 1933"},
-		{name: "May 1937 - June 1938"},
-		{name: "February 1945 - October 1945"},
-		{name: "Novemeber 1948 - October 1949"},
-		{name: "July 1953 - May 1954"}
+		{name: "Great Depression: August 1929 - March 1933", color: "purple"},
+		{name: "May 1937 - June 1938", color: "green"},
+		{name: "February 1945 - October 1945", color: "blue"},
+		{name: "Novemeber 1948 - October 1949", color: ""},
+		{name: "July 1953 - May 1954", color: ""}
 	];
 	$scope.expansions = [
-		{name: "October 1945 - Novemeber 1948"},
-		{name: "October 1949 - July 1953"},
-		{name: "May 1954 - August 1957"},
-		{name: "April 1958 - April 1960"},
-		{name: "Feb 1961 - December 1969"}
+		{name: "October 1945 - Novemeber 1948", color: ""},
+		{name: "October 1949 - July 1953", color: ""},
+		{name: "May 1954 - August 1957", color: ""},
+		{name: "April 1958 - April 1960", color: ""},
+		{name: "Feb 1961 - December 1969", color: ""}
 	];
 	$scope.currentQuestion = 1;
 	$scope.showQuestion = function(num) {
