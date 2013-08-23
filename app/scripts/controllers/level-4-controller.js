a3a746cb0b7399c86fa946d42d8d2f8f0401917c
Brandon Brown <bbrown@johnmcneilstudio.com>
2013-08-22 19:01:17 -0700

    implmented selection behavior on both recessions and expansions

--- app/scripts/controllers/level-4-controller.js
+++ app/scripts/controllers/level-4-controller.js
@@ -27,26 +27,43 @@ unemploymentApp.controller('Level4Ctrl', ['$scope',  function($scope ) {
 	$scope.expansionsIsCollapsed = false;
 	$scope.availableSelectionColors = ['purple','green','blue'];
 	$scope.selectedPeriods = [];
-	$scope.togglePeriodSelect = function(period, ngRepeatIndex) {
+	$scope.currentSelectionList;
+	$scope.toggleSelectedPeriod = function(period, list, ngRepeatIndex) {
+		if($scope.currentSelectionList && $scope.currentSelectionList != list ) {
+			$scope.resetSelections();
+		} 
+		$scope.currentSelectionList = list;
 	    if(period.selected) {
-	    	period.selected = false;
-			$scope.selectedPeriods.splice(_.indexOf($scope.selectedPeriods, period),1);
-			period.selectedOrderNum = "";
-	    	$scope.availableSelectionColors.push(period.color);
-			period.color = "";
+	    	$scope.deselectPeriod(period);
 	    } else {
 	    	if($scope.selectedPeriods.length < 3) {
-		    	period.selected = true;
-		    	$scope.selectedPeriods.push(period);
-		    	period.ngRepeatIndex = ngRepeatIndex;
-		    	$scope.selectedPeriods = _.sortBy($scope.selectedPeriods, 'ngRepeatIndex');
-		    	_.each($scope.selectedPeriods, function(period, index, list) {
-		    		period.selectedOrderNum = index + 1;
-		    	});
-		    	period.color = $scope.availableSelectionColors.shift();
+	    		$scope.selectPeriod(period, ngRepeatIndex);
 		    } else {
 		    	return;
 		    }
 	    }
+	}
+	$scope.selectPeriod = function(period, ngRepeatIndex) {
+		period.selected = true;
+    	$scope.selectedPeriods.push(period);
+    	period.ngRepeatIndex = ngRepeatIndex;
+    	$scope.selectedPeriods = _.sortBy($scope.selectedPeriods, 'ngRepeatIndex');
+    	_.each($scope.selectedPeriods, function(period, index, list) {
+    		period.selectedOrderNum = index + 1;
+    	});
+    	period.color = $scope.availableSelectionColors.shift();
+	}
+	$scope.deselectPeriod = function(period) {
+			period.selected = false;
+			$scope.selectedPeriods.splice(_.indexOf($scope.selectedPeriods, period),1);
+			period.selectedOrderNum = "";
+	    	$scope.availableSelectionColors.push(period.color);
+			period.color = "";
+	}
+	$scope.resetSelections = function() {
+		var unSelectedPeriods = $scope.selectedPeriods.slice(0); //had to make a copy by value of the array
+		_.each(unSelectedPeriods, function(period, index, list) {
+    		$scope.deselectPeriod(period);
+    	});
 	}
 }]);
