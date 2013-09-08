edd341448784022ce0fa578a206e8e72a840de64
Brandon Brown <bbrown@johnmcneilstudio.com>
2013-09-07 17:14:48 -0700

    toggling the grid and graph views working

--- app/scripts/controllers/level-4-controller.js
+++ app/scripts/controllers/level-4-controller.js
@@ -41,11 +41,25 @@ unemploymentApp.controller('Level4Ctrl', ['$scope', 'unemploymentDataService',  
 		$scope.currentQuestion++; 
 	}
 
-	$scope.recessionsIsCollapsed = false;
+	$scope.recessionsIsCollapsed = true;
 	$scope.expansionsIsCollapsed = true;
 	$scope.availableSelectionColors = ['purple','green','blue'];
 	$scope.selectedPeriods = [];
 	$scope.currentSelectionList;
+
+	$scope.showGridOrGraph = "graph";
+	$scope.toggleGridAndGraphViews = function(view) {
+		console.log(view);
+        if (view === "graph") {
+        	$scope.showGridOrGraph = "graph";
+        } else if (view === "grid") {
+        	$scope.showGridOrGraph = "grid";        	
+        }
+    };
+
+    $scope.foo = function(view) {
+    	console.log(view);
+    }
 
 	$scope.showMonthDialPopover = false;
 	$scope.toggleShowMonthDialPopover = function(state) {
