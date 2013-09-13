af0f382d69434531a25c8e9a5c43c02ddce71320
Brandon Brown <bbrown@johnmcneilstudio.com>
2013-09-12 18:18:50 -0700

    date picker standin for month dial is working

--- app/scripts/controllers/level-3-controller.js
+++ app/scripts/controllers/level-3-controller.js
@@ -4,8 +4,8 @@ unemploymentApp.controller('Level3Ctrl', ['$scope', '$timeout', 'mapDataService'
 	// $scope.currentState = "Choose a State";
 	// $scope.currentCounty1 = "Choose County 1";
 	// $scope.currentCounty2 = "Choose County 2";
-	// $scope.currentStep = {val: 1};
-	// $scope.currentScale = {val: "nation"};
+	$scope.currentStep = {val: 1};
+	$scope.currentScale = {val: "nation"};
 	// $scope.currentView = {val: "graph"};
 	// $scope.usStates = [
 	//     { name: 'ALABAMA', abbreviation: 'AL'},
@@ -68,45 +68,51 @@ unemploymentApp.controller('Level3Ctrl', ['$scope', '$timeout', 'mapDataService'
 	//     { name: 'WISCONSIN', abbreviation: 'WI'},
 	//     { name: 'WYOMING', abbreviation: 'WY' }
 	// ];
-	// $scope.submitResponse = function() {
-	// 	//TODO grading and feedback happen here
-	// 	$scope.currentStep.val += 1; 
-	// 	//TODO after step 4 go to level 4
-	// }
-	// $scope.setCurrentScale = function(scale) {
-	// 	$scope.currentScale.val = scale;
-	// }
+	$scope.submitResponse = function() {
+		//TODO grading and feedback happen here
+		$scope.currentStep.val += 1; 
+		//TODO after step 4 go to level 4
+	}
+	$scope.setCurrentScale = function(scale) {
+		$scope.currentScale.val = scale;
+	}
 	// $scope.setCurrentView = function(view) {
 	// 	$scope.currentView.val = view;
 	// }
 
 
 
-	$scope.year = '2000';
-	$scope.month = '01';
+	//$scope.year = '2000';
+	$scope.year = {val: '2000'};
+	//$scope.month = '01';
+	$scope.month = {val: '01'};
 	$scope.years = [];
 	$scope.months = [];
 	for (var y=2000; y<=2013; y++) {$scope.years.push(y)};
 	for (var m=1; m<=12; m++) {$scope.months.push(m)};
 	
-	$scope.regionName = "United States";
+	//$scope.regionName = "United States";
+	$scope.regionName = {val: "United States"};
 	$scope.subRegionData = 'Waiting';
+	//$scope.subRegionData = {val: 'Waiting'};
 	
 	$scope.regions = ['United States'];
 	$scope.countyList = [];
-	$scope.county1 = '';
-	$scope.county2 = '';
+	//$scope.county1 = '';
+	$scope.county1 = {val: ''};
+	//$scope.county2 = '';
+	$scope.county2 = {val: ''};
 	
 	$scope.setRegionName = function(region) {
-		$scope.regionName = region;
+		$scope.regionName.val = region;
 	}
 	
 	$scope.setCounty1 = function(county) {
-		$scope.county1 = county;
+		$scope.county1.val = county;
 	}
 	
 	$scope.setCounty2 = function(county) {
-		$scope.county2 = county;
+		$scope.county2.val = county;
 	}
 
 	$scope.dataForCounty = function(countyName) {
@@ -118,39 +124,35 @@ unemploymentApp.controller('Level3Ctrl', ['$scope', '$timeout', 'mapDataService'
 	}
 	
 	$scope.updateData = function(regionChanged) {
-		mapDataService.getRegionalDataForDate($scope.regionName, $scope.year+'-'+$scope.month+'-01', function(data) {
+		mapDataService.getRegionalDataForDate($scope.regionName.val, $scope.year.val+'-'+$scope.month.val+'-01', function(data) {
 			$scope.usValue = data.us.value;
-			$scope.regionName = data.region.name;
+			$scope.regionName.val = data.region.name;
 			$scope.regionValue = data.region.value;
 			$scope.subRegionData = data.subRegions;
-			if ($scope.regionName=='United States') {
+			if ($scope.regionName.val=='United States') {
 				$scope.regions = ['United States'].concat(data.subRegionNames);
 				$scope.subRegionType = 'States';
 			} else if (regionChanged) {
-				console.log("regionChanged");
 				$scope.subRegionType = 'Counties';
 				$scope.countyList = data.subRegionNames;
-				$scope.county1 = '';
-				$scope.county2 = '';
+				$scope.county1.val = '';
+				$scope.county2.val = '';
 			}
-			if ($scope.county1!='') {
-				$scope.county1Value = $scope.dataForCounty($scope.county1);
-				console.log($scope.county1Value);
+			if ($scope.county1.val!='') {
+				$scope.county1Value = $scope.dataForCounty($scope.county1.val);
 			}
-			if ($scope.county2!='') {
-				$scope.county2Value = $scope.dataForCounty($scope.county2);
+			if ($scope.county2.val!='') {
+				$scope.county2Value = $scope.dataForCounty($scope.county2.val);
 			}
 	});
 	};
 	
-	$scope.$watch('regionName', function() {
+	$scope.$watch('regionName.val', function() {
 		$scope.updateData(true);
-		console.log("$scope.updateData(true);");
 		});
 	
-	$scope.$watch('year+"-"+month', function() {
+	$scope.$watch('year.val+"-"+month.val', function() {
 		$scope.updateData(false);
-		console.log("$scope.updateData(false);");
 		});
 		
 	// tools for charting the data...
@@ -159,22 +161,20 @@ unemploymentApp.controller('Level3Ctrl', ['$scope', '$timeout', 'mapDataService'
 	$scope.county1ChartData = [];
 	$scope.county2ChartData = [];
 	
-	$scope.$watch('county1+" "+county2', function() {
-		if ($scope.county1!='') {
-			$scope.county1Value = $scope.dataForCounty($scope.county1);
-			console.log("county1Value = "+$scope.county1Value);
+	$scope.$watch('county1.val+" "+county2.val', function() {
+		if ($scope.county1.val!='') {
+			$scope.county1Value = $scope.dataForCounty($scope.county1.val);
 		}
-		if ($scope.county2!='') {
-			$scope.county2Value = $scope.dataForCounty($scope.county2);
-			console.log("county2Value = "+$scope.county2Value);
+		if ($scope.county2.val!='') {
+			$scope.county2Value = $scope.dataForCounty($scope.county2.val);
 		}
-		if ($scope.county1=='' || $scope.county2=='') {
+		if ($scope.county1.val=='' || $scope.county2.val=='') {
 			$scope.usChartData = [];
 			$scope.stateChartData = [];
 			$scope.county1ChartData = [];
 			$scope.county2ChartData = [];
 		} else {
-			mapDataService.getChartableData($scope.regionName, $scope.county1, $scope.county2, function(result) {
+			mapDataService.getChartableData($scope.regionName, $scope.county1.val, $scope.county2.val, function(result) {
 				$scope.usChartData = result.us;
 				console.log("usChartData = "+$scope.usChartData);
 				$scope.stateChartData = result.state;
