7b595dd62f1233bd90400d90525c9fb644c07808
Brandon Brown <bbrown@johnmcneilstudio.com>
2013-09-12 18:19:41 -0700

    adding new files

---
+++ app/scripts/filters/filters.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/* Filters */
+
+angular.module('unemploymentApp.filters', ['unemploymentApp.services']).
+  filter('pointFormat', [function() {
+    return function(val) {
+			if (val!=undefined) {
+				var d = Date.dateFromFloat(val[0]);
+				return [d.getFullYear()+'-'+d.getShortMonthName(), val[1]];
+			}
+    }
+  }])
+  .filter('percentageFilter', [function() {
+    return function (input) {
+    	if(input === undefined || input === 'undefined' || input === null || input <= 0) return 'NA';
+      return input + '%';
+    };
+  }]);
