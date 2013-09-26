8ba2454f2479f3249f237066125a0254fd5bf8b2
Brandon Brown <bbrown@johnmcneilstudio.com>
2013-09-25 17:36:53 -0700

    created an event that closes other popovers before displaying the current one

--- app/scripts/directives/popover-directives.js
+++ app/scripts/directives/popover-directives.js
@@ -3,23 +3,28 @@
 angular.module('directives.ue.popovers', [])
 .directive('customPopover',['$position', '$timeout', function($position, $timeout){
 	return {
-		restrict: 'AC',
+		restrict: 'C',
 		replace: false,
 		link: function(scope, element, attrs) {
 			var showEvent = attrs.showEvent;
-			console.log(attrs);
 			var showing = false;
 			scope.$on(showEvent, function() {
+				scope.$broadcast('closeOtherPopovers', element);
 				if(showing) return;
 				showing = true;
 				$(element).fadeIn('slow', function() {
-					$('body').bind('click.popover', function() {
+					$('body').bind('click.popover keyup.popover', function() {
 						$(element).fadeOut('slow');
-						$(this).unbind('click.popover');
+						$(this).unbind('click.popover keyup.popover');
 						showing = false;
 						return;
 					});
 				});
+			});
+			scope.$on('closeOtherPopovers', function(e, src) {
+				if(element === src) return;
+				showing = false;
+				$(element).fadeOut('slow');
 			});
 		}
 	}
